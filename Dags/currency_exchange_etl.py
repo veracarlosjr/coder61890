941f061ac924573d0500d4d5b4233173a7d93349
Carlos Vera <124402128+veracarlosjr@users.noreply.github.com>
2024-08-30 21:14:37 +0000

    Agregar archivos para la tercera entrega: Docker, Airflow y otros cambios

---
+++ Dags/currency_exchange_etl.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import subprocess
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def run_etl():
+    subprocess.run(["python", "/app/scripts/script.py"])
+
+dag = DAG(
+    'daily_etl_dag',
+    default_args=default_args,
+    description='A simple daily ETL DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+run_etl_task = PythonOperator(
+    task_id='run_etl',
+    python_callable=run_etl,
+    dag=dag,
+)
+
+
+
